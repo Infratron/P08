6a8175d0a07998a8da443fc5a1d4b93909f0deab
Matteo Giannone <matteogianone@gmail.com>
2023-04-05 18:07:26 +0200

    pagina completata con javascrypt

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 
 let MainNavbar = document.querySelector('#MainNavbar');
 
+// catturo elementi HTML nello scope globale
+let Primospan = document.querySelector('#Primospan')
+let Secondospan = document.querySelector('#Secondospan')
+let Terzospan = document.querySelector('#Terzospan')
+
 // creo l'evento per lo scroll della navbar
 
 window.addEventListener('scroll', ()=>{
@@ -15,3 +20,40 @@ window.addEventListener('scroll', ()=>{
         MainNavbar.classList.add('transnav');
     }
 });
+
+function createinterval (finalnumber, element){
+
+    let counter = 0;
+
+    let interval = setInterval(()=>{
+
+    if (counter < finalnumber){
+        counter++
+        element.innerHTML = counter;
+    } else {
+        clearInterval(interval)
+    }
+    }, 1)
+}
+
+// variabile appoggio per cessare incremento numeri
+
+let check = true;
+
+
+let observed = new IntersectionObserver(
+    
+    (entries)=>{
+
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting && check == true){
+            createinterval(1500 , Primospan);
+            createinterval(2500 , Secondospan);
+            createinterval(850 , Terzospan);
+            check = false;
+        }
+    })
+    
+    }
+)
+observed.observe(Primospan);
